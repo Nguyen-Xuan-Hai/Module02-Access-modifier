3c247f74dad34921a2d6bf17545d673fddec7606
Nguyen-Xuan-Hai <kute3bino@gmail.com>
2021-01-31 17:12:02 +0700

    Bai-tap

--- product-manager/index.php
+++ product-manager/index.php
@@ -8,7 +8,6 @@ use Models\Product;
 $productManager = new ProductManager();
 $productManager->add(new Product("Laptop"));
 $productManager->add(new Product("Mobile"));
-
 $products = $productManager->getProducts();
 foreach ($products as $product) {
     echo $product->getName() ."<br/>";
